4a54a92c2824e5957d392ca5a05f4b3e62bedd89
Vegeku <vegekusheron@gmail.com>
2024-03-03 17:09:05 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,9 @@
+// message board app
+// stage 2: add route for getting messages
+import express from 'express';
+
+const app = express();
+app.use(express.static('javascript'));
+
+
+app.listen(8080);
