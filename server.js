fb2a808e755832264ebf025a4c4b74a54aae077c
Vegeku <vegekusheron@gmail.com>
2024-03-04 10:06:58 +0000

    starting from scratch

--- server.js
+++
@@ -1,9 +0,0 @@
-// message board app
-// stage 2: add route for getting messages
-import express from 'express';
-
-const app = express();
-app.use(express.static('javascript'));
-
-
-app.listen(8080);
