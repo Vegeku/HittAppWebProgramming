eb9340078cf0645c4e172f805f20eca5255864f6
Vegeku <vegekusheron@gmail.com>
2024-03-14 13:52:31 +0000

    added functionality to cancel button

--- frontend/add_workout.js
+++ frontend/add_workout.js
@@ -6,7 +6,26 @@ const workoutPage = document.querySelector('#workoutPage');
 
 const workoutList = document.querySelector("#workouts");
 
-addWorkoutBtn.addEventListener("click", e => {
-    const cloned = workoutPage.content.cloneNode(true);
-    workoutList.append(cloned);
-});
+function home() {
+    addWorkoutToDashboard()
+
+}
+
+function addWorkoutToDashboard() {
+
+    addWorkoutBtn.addEventListener("click", e => {
+        const cloned = workoutPage.content.cloneNode(true);
+        workoutList.append(cloned);
+        addWorkoutBtn.disabled = true;
+        const cancel = document.querySelector("#cancelWorkout");
+        const workout = document.querySelector("#workout");
+        cancel.addEventListener("click", e => {
+            workout.remove();
+            addWorkoutBtn.disabled = false;
+        })
+    });
+}
+
+
+
+home();
